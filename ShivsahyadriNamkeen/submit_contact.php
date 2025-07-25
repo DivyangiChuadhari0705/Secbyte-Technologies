90550aead53563b116e7d0aa28fc396e22c03d37
DivyangiChuadhari0705 <divyangichaudhari0705@gmail.com>
2025-07-25 20:02:22 +0530

    Update submit_contact.php

--- ShivsahyadriNamkeen/submit_contact.php
+++ ShivsahyadriNamkeen/submit_contact.php
@@ -1,28 +1,25 @@
 <?php
-// 1. Database कनेक्शन (no password वापरून)
 $servername = "localhost";
 $username = "root";
-$password = "";  // ← इथे password जर लागतो तर द्या
+$password = "";  
 $dbname = "shivsahyadri_db1";
 $port = 4306; 
-// 2. Connection बनवा
+
 $conn = new mysqli($servername, $username, $password, $dbname,$port);
 
-// 3. Error चेक करा
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 
-
-// Form data safely घ्या
 $name     = isset($_POST['name']) ? $_POST['name'] : '';
 $email    = isset($_POST['email']) ? $_POST['email'] : '';
 $phone    = isset($_POST['phone']) ? $_POST['phone'] : '';
 $location = isset($_POST['location']) ? $_POST['location'] : '';
 $message  = isset($_POST['message']) ? $_POST['message'] : '';
 
-// 5. SQL insert करा
+
 $sql = "INSERT INTO contact_messages (name, email, phone , location, message)
 VALUES ('$name', '$email', '$phone', '$location', '$message')";
 
